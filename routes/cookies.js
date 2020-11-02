552977883e2755765c28e5e520f4628c1808bab1
nicolas <nicolas.mercier570@gmail.com>
2020-11-02 17:08:38 +0100

    Structuring sql queries

--- routes/cookies.js
+++ routes/cookies.js
@@ -5,7 +5,11 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   con.query(
-    'select c.id, c.name, c.description, c.price, c.image, GROUP_CONCAT(ing.name) ingredients from cookie c join cookie_ingredients c_i on c.id = c_i.cookie_id join ingredient ing on ing.id = c_i.ingredient_id group by c.id',
+    `SELECT c.id, c.name, c.description, c.price, c.image, GROUP_CONCAT(ing.name) ingredients
+     FROM cookie c
+     JOIN cookie_ingredients c_i ON c.id = c_i.cookie_id
+     JOIN ingredient ing ON ing.id = c_i.ingredient_id
+     group by c.id`,
     (err, result) => {
       if (err) {
         return res.status(500).send('Something went wrong');
@@ -18,14 +22,19 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   const { id } = req.params;
   con.query(
-    'select c.id, c.name, c.description, c.price, c.image, GROUP_CONCAT(ing.name) ingredients from cookie c join cookie_ingredients c_i on c.id = c_i.cookie_id join ingredient ing on ing.id = c_i.ingredient_id where c.id = ? group by c.id ',
+    `SELECT c.id, c.name, c.description, c.price, c.image, GROUP_CONCAT(ing.name) ingredients
+     FROM cookie c
+     JOIN cookie_ingredients c_i ON c.id = c_i.cookie_id
+     JOIN ingredient ing ON ing.id = c_i.ingredient_id
+     WHERE c.id = ?
+     GROUP by c.id`,
     [id],
     (err, result) => {
       if (err) {
         return res.status(500).send('Something went wrong');
       }
       if (result.length === 0) {
-        return res.status(404).send('Employee not found');
+        return res.status(404).send('Cookie not found');
       }
       return res.json(result);
     }
