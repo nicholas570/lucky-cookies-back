93d6878f416565dd2147ff7017308af7168a83cb
nicolas <nicolas.mercier570@gmail.com>
2020-11-07 23:40:34 +0100

    adding cors lib

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const cookies = require('./routes/cookies');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(cors());
 app.use(express.json());
 app.use(
   express.urlencoded({
