1464eed07c7cb59ed0f4536feb43913b39def5ca
nicolas <nicolas.mercier570@gmail.com>
2020-11-25 21:01:54 +0100

    update from connection to pool

--- database/conf.js
+++ database/conf.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 
 require('dotenv').config();
 
-const con = mysql.createConnection({
+const con = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
