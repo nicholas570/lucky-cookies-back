4aa713b331ed704bebbc7e4d33bc92a985a00648
nicolas <nicolas.mercier570@gmail.com>
2020-12-01 12:00:11 +0100

    validated newsletter route

--- routes/contacts.js
+++ routes/contacts.js
@@ -4,7 +4,7 @@ const Contacts = require('../models/contacts');
 
 const router = express.Router();
 
-const validateContact = require('../middlewares /checkcontact');
+const validateContact = require('../middlewares/validations/checkContact');
 
 router.post('/', validateContact, Contacts.send);
 
